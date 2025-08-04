2ea111fb4a0ebac469fdebaa97b02937c79ae8d5
AlphaGaurav13 <gauravkkr345@gmail.com>
2025-08-04 21:56:08 +0530

    My First is Ready for production

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("GauravCodingThunder");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/gaurav', (req, res) => {
+  res.send('Gauravdotcom')
+})
+
+app.get('/login', (req, res) => {
+  res.send('Login Kar le bhai :)')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${port}`)
+})
